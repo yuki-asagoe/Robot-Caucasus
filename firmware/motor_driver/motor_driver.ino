fb73fc1a3cced8de44047726763c6ada5fd66d30
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-04-27 17:56:55 +0900

    implement motor driver firmware

---
+++ firmware/motor_driver/motor_driver.ino
@@ -0,0 +1,82 @@
+#include <mcp_can.h>
+#include <motor.h>
+#include <can_communication.h>
+
+#include "variables.h"
+
+void on_receive_can(uint16_t, const int8_t*, uint8_t);
+void warn(char*);
+
+unsigned long long last_can_timestamp=0;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(STAT_LED1, OUTPUT); // CAN メッセージ受信したら光る
+  pinMode(STAT_LED2, OUTPUT); // CAN制御信号が1秒途切れ(てセーフティが発動し)たら光る
+
+  motorInit();
+
+  CanCom.begin(can_address, CAN_250KBPS); 
+  CanCom.onReceive(on_receive_can);
+
+  Serial.println("Ready to Drive - motors in Caucasus");
+  Serial.print("My can destination id is\"");
+  Serial.print(can_address,DEC);
+  Serial.println("\". Git Repository - https://github.com/yuki-asagoe/Robot-Caucasus");
+}
+
+void loop(){
+  //データ受信を確認し必要ならonReceiveで登録したリスナを呼び出す
+  CanCom.tasks();
+  unsigned long long now=millis();
+  if(now - last_can_timestamp > 1000){ //セーフティストッパー
+    motorStop(MOTOR1); motorStop(MOTOR2);
+    digitalWrite(STAT_LED2,HIGH);
+  }else{
+    digitalWrite(STAT_LED2,LOW);    
+  }
+  if(now -last_can_timestamp < 100){
+    digitalWrite(STAT_LED1,HIGH);
+  }else{
+    digitalWrite(STAT_LED1,LOW);    
+  }
+}
+
+void on_receive_can(uint16_t std_id, const int8_t *data, uint8_t len) {
+  last_can_timestamp=millis();
+  uint8_t msg_type = CanCommunication::getDataTypeFromStdId(std_id);
+  uint8_t dest = CanCommunication::getDestFromStdId(std_id);  
+  
+  if(dest!=can_address || dest!=0){return;}
+  
+  switch(msg_type){
+    case CAN_DATA_TYPE_COMMAND:{
+      if(len!=4){
+        warn("illegal data length");
+        return;
+      }
+      if(data[0]==0){motorStop(1);}
+      else if(data[0]==255){motorFree(1);}
+      else if(data[0]>0){motorWrite(1,(uint8_t)data[1]);}
+      else {motorWrite(1,-(uint8_t)data[1]);}
+      if(data[2]==0){motorStop(3);}
+      else if(data[2]==255){motorFree(3);}
+      else if(data[2]>0){motorWrite(2,(uint8_t)data[3]);}
+      else {motorWrite(2,-(uint8_t)data[3]);}
+      break;      
+    }
+    case CAN_DATA_TYPE_EMERGENCY:{
+      motorStop(1);
+      motorStop(2);
+      break;
+    }
+  }
+}
+
+void warn(char* msg){
+  Serial.print("Warning: ");
+  Serial.print(msg);
+  Serial.print(" -by motor drive(id:");
+  Serial.print(can_address,DEC);
+  Serial.println(")");
+}
