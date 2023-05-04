b4f3d6d5d77347799a5e6acbec3a64166c7ba899
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-05-04 11:08:30 +0900

    add simple wimm implementation

---
+++ wimm-implementation/Wimm.Machines.Impl.Caucasus/Caucasus.cs
@@ -0,0 +1,91 @@
+﻿using Wimm.Machines.Audio;
+using Wimm.Machines.Impl.Caucasus.Audio;
+using Wimm.Machines.TpipForRasberryPi;
+using Wimm.Machines.Video;
+using Wimm.Machines;
+using Wimm.Machines.Component;
+using System.Windows.Interop;
+using System.Collections.Immutable;
+using Wimm.Machines.Impl.Caucasus.Can;
+using Wimm.Machines.Impl.Caucasus.Component;
+
+namespace Wimm.Machines.Impl.Caucasus
+{
+    public class Caucasus : TpipForRasberryPiMachine
+    {
+        public override string Name => "コーカサス";
+
+        public override Speaker Speaker { get; }= new CaucasusSpeaker();
+
+        public override Microphone Microphone => new CaucasusMicrophone();
+
+        public override Camera Camera { get; } = new Tpip4Camera(
+            "カメラ1"
+        );
+        IEnumerable<CanCommunicationUnit> CanMessageFrames { get; }
+        public Caucasus(string tpipIpAddress, HwndSource hwnd) :base(tpipIpAddress, hwnd)
+        {
+            if (Camera is Tpip4Camera camera){ hwnd.AddHook(camera.WndProc); }
+            (CanMessageFrames,StructuredModules)  = CreateStructuredModule();
+        }
+        public Caucasus() : base()
+        {
+            (CanMessageFrames,StructuredModules) = CreateStructuredModule();
+        }
+        private static (IEnumerable<CanCommunicationUnit>,ModuleGroup) CreateStructuredModule()
+        {
+            CanCommunicationUnit CrawlersCanFrame = new CanCommunicationUnit(
+                new()
+                {
+                    DestinationAddress = (CanDestinationAddress)1,
+                    SourceAddress = CanDestinationAddress.BroadCast,
+                    MessageType = CanDataType.Command
+                },
+                4
+            );
+            var canFrames = new CanCommunicationUnit[]
+            {
+                CrawlersCanFrame
+            };
+            var structuredModules= new ModuleGroup("modules",
+                ImmutableArray.Create(
+                    new ModuleGroup("crawlers",
+                        ImmutableArray.Create<ModuleGroup>(),
+                        ImmutableArray.Create<Module>(
+                            new CaucasusMotor(
+                                "right","機動用右クローラー",
+                                CrawlersCanFrame,CaucasusMotor.DriverPort.M1
+                            ),
+                            new CaucasusMotor(
+                                "left", "機動用左クローラー",
+                                CrawlersCanFrame, CaucasusMotor.DriverPort.M2
+                            )
+                        )
+                    )
+                ),
+                ImmutableArray.Create<Module>()
+            );
+            return (canFrames, structuredModules);
+        }
+        public override ControlProcess StartControlProcess()
+        {
+            return base.StartControlProcess();
+        }
+        class CaucasusControlProcess : ControlProcess
+        {
+            Caucasus Caucasus { get; }
+            public CaucasusControlProcess(Caucasus caucasus)
+            {
+                Caucasus = caucasus;
+            }
+            public override void Dispose()
+            {
+                foreach(var message in Caucasus.CanMessageFrames)
+                {
+                    message.Send();
+                }
+                base.Dispose();
+            }
+        }
+    }
+}
---
+++ wimm-implementation/Wimm.Machines.Impl.Caucasus/Component/CaucasusMotor.cs
@@ -0,0 +1,38 @@
+﻿using Wimm.Machines.Component;
+using Wimm.Machines.Impl.Caucasus.Can;
+
+namespace Wimm.Machines.Impl.Caucasus.Component
+{
+    internal class CaucasusMotor : Motor
+    {
+        internal enum DriverPort { M1=0,M2=1 }
+        public CaucasusMotor(string name, string description, CanCommunicationUnit canMessage,DriverPort port) : base(name, description)
+        {
+            Port = port;
+            if (canMessage.Data.Length != 4)
+            {
+                throw new ArgumentException($"与えられた<{nameof(CanCommunicationUnit)}>インスタンスのデータのサイズが4byteではありません。");
+            }
+            CanMessageFrame = canMessage;
+        }
+        private DriverPort Port { get; }
+        private CanCommunicationUnit CanMessageFrame { get; }
+
+        private void RotationImpl(double speed)
+        {
+            speed = Math.Clamp(speed, -1, 1);
+            CanMessageFrame.Data[(int)Port * 2] = (byte)Math.Sign(speed); //回転方向決定
+            CanMessageFrame.Data[(int)Port * 2 + 1] = (byte)(byte.MaxValue*Math.Abs(speed)); // 回転速度決定
+        }
+
+        public override Feature<Action<double>> RotationFeature => new Feature<Action<double>>(
+            Motor.RotationFeatureName,
+            "モーターを回転させます。\n\n[引数]\n- double speed - 範囲 -1 ~ 1 です。",
+            RotationImpl
+        );
+
+        public override string ModuleName => "コーカサス モーター";
+
+        public override string ModuleDescription => "コーカサス搭載のモータードライバデフォルト実装";
+    }
+}
