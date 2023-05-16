8adf1397efd45932fa499fa94b08439475334b57
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-05-16 20:42:08 +0900

    add tpip3 implementation

---
+++ wimm-implementation/Wimm.Machines.Impl.Caucasus.Tpip3/Audio/CaucasusSpeaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wimm.Machines.Audio;
+
+namespace Wimm.Machines.Impl.Caucasus.Tpip3.Audio
+{
+    internal class CaucasusSpeaker : Speaker
+    {
+    }
+}
---
+++ wimm-implementation/Wimm.Machines.Impl.Caucasus.Tpip3/Caucasus.cs
@@ -0,0 +1,94 @@
+﻿using Wimm.Machines.Audio;
+using Wimm.Machines.Tpip3;
+using Wimm.Machines.Video;
+using Wimm.Machines;
+using Wimm.Machines.Component;
+using System.Windows.Interop;
+using System.Collections.Immutable;
+using Wimm.Machines.Impl.Caucasus.Tpip3.Audio;
+using Wimm.Machines.Impl.Caucasus.Tpip3.Can;
+using Wimm.Machines.Impl.Caucasus.Tpip3.Component;
+
+namespace Wimm.Machines.Impl.Caucasus.Tpip3
+{
+    public class Caucasus : Tpip3Machine
+    {
+        public override string Name => "コーカサス";
+
+        public override Speaker Speaker { get; } = new CaucasusSpeaker();
+
+        public override Microphone Microphone => new CaucasusMicrophone();
+
+        public override Camera Camera { get; } = new Tpip3Camera(
+            "カメラ1"
+        );
+        IEnumerable<CanCommunicationUnit> CanMessageFrames { get; }
+        public Caucasus(string tpipIpAddress, HwndSource hwnd) : base(tpipIpAddress, hwnd)
+        {
+            if (Camera is Tpip3Camera camera) { hwnd.AddHook(camera.WndProc); }
+            (CanMessageFrames, StructuredModules) = CreateStructuredModule(() => SpeedModifier);
+        }
+        public Caucasus() : base()
+        {
+            (CanMessageFrames, StructuredModules) = CreateStructuredModule(() => SpeedModifier);
+        }
+        private static (IEnumerable<CanCommunicationUnit>, ModuleGroup) CreateStructuredModule(Func<double> speedModifierProvider)
+        {
+            CanCommunicationUnit CrawlersCanFrame = new CanCommunicationUnit(
+                new()
+                {
+                    DestinationAddress = (CanDestinationAddress)1,
+                    SourceAddress = CanDestinationAddress.BroadCast,
+                    MessageType = CanDataType.Command
+                },
+                4
+            );
+            var canFrames = new CanCommunicationUnit[]
+            {
+                CrawlersCanFrame
+            };
+
+            var structuredModules = new ModuleGroup("modules",
+                ImmutableArray.Create(
+                    new ModuleGroup("crawlers",
+                        ImmutableArray.Create<ModuleGroup>(),
+                        ImmutableArray.Create<Module>(
+                            new CaucasusMotor(
+                                "right", "機動用右クローラー",
+                                CrawlersCanFrame, CaucasusMotor.DriverPort.M1,
+                                speedModifierProvider
+                            ),
+                            new CaucasusMotor(
+                                "left", "機動用左クローラー",
+                                CrawlersCanFrame, CaucasusMotor.DriverPort.M2,
+                                speedModifierProvider
+                            )
+                        )
+                    )
+                ),
+                ImmutableArray.Create<Module>()
+            );
+            return (canFrames, structuredModules);
+        }
+        public override ControlProcess StartControlProcess()
+        {
+            return base.StartControlProcess();
+        }
+        class CaucasusControlProcess : ControlProcess
+        {
+            Caucasus Caucasus { get; }
+            public CaucasusControlProcess(Caucasus caucasus)
+            {
+                Caucasus = caucasus;
+            }
+            public override void Dispose()
+            {
+                foreach (var message in Caucasus.CanMessageFrames)
+                {
+                    message.Send();
+                }
+                base.Dispose();
+            }
+        }
+    }
+}
