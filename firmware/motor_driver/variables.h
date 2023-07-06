f18d7893e0ec0b199ee0bcb4283899c72f97e2b6
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-07-06 18:11:19 +0900

    add two limit input for each motors

--- firmware/motor_driver/variables.h
+++ firmware/motor_driver/variables.h
@@ -1,6 +1,15 @@
 const int CAN_Speed=CAN_250KBPS;
 const uint8_t CAN_Self_Address=1;
 
-const bool Limit_Switch_For_M1_Enabled=false;
-const bool Limit_Switch_For_M2_Enabled=false;
+enum class Limit_Switch_Type : int{
+  //無効化
+  Disabled=0,
+  //INPUT[X]_1がHIGHならM[X]を強制停止
+  Single=1,
+  //INPUT[X]_1,INPUT[X]_2のどちらかがHIGHならM[X]を強制停止
+  OR=2
+};
+
+const Limit_Switch_Type Limit_Switch_For_M1 = Limit_Switch_Type::Disabled;
+const Limit_Switch_Type Limit_Switch_For_M2 = Limit_Switch_Type::Disabled;
 const bool Detailed_Logging_Enabled=true;
