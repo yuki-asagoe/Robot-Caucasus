cb4364fee7cd2d77e758e00e7869d712b6ae4943
yuki-asagoe <104046935+yuki-asagoe@users.noreply.github.com>
2023-07-08 11:03:11 +0900

    add container

---
+++ wimm-implementation/Wimm.Machines.Impl.Caucasus/Component/CaucasusContainer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wimm.Machines.Impl.Caucasus.Component
+{
+    internal class CaucasusContainer : Module
+    {
+        CaucasusMotor BeltRotateMotor { get; }
+        CaucasusMotor MoveContainerMotor { get; }
+        public CaucasusContainer(string name, string description,CaucasusMotor beltRotateMotor,CaucasusMotor moveContainerMotor) : base(name, description)
+        {
+            BeltRotateMotor = beltRotateMotor;
+            MoveContainerMotor = moveContainerMotor;
+            Features = ImmutableArray.Create(
+                new Feature<Delegate>("rotate_belt","Action<speed:double> : ベルトを回転させます。",RotateBelt),
+                new Feature<Delegate>("move_container","Action<speed:double> : コンテナを出し入れします。",MoveContainer)
+            );
+        }
+        public void RotateBelt(double speed)
+        {
+            BeltRotateMotor.Rotate(speed);
+        }
+        public void MoveContainer(double speed)
+        {
+            MoveContainerMotor.Rotate(speed);
+        }
+
+        public override string ModuleName => "コーカサス 救助者格納用コンテナ";
+
+        public override string ModuleDescription => "コーカサスの救助者格納用のコンテナです";
+    }
+}
